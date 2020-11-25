adb64006b7db175add8cbd8c807347c6858bae0b
jianghe <he.jiang@microfun.com>
2020-11-26 00:18:58 +0800

    [lua】库导入

--- cocos2dx/MyCocos/Classes/AppDelegate.cpp
+++ cocos2dx/MyCocos/Classes/AppDelegate.cpp
@@ -83,9 +83,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("MyCocos", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("test", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("MyCocos");
+        glview = GLViewImpl::create("test");
 #endif
         director->setOpenGLView(glview);
     }
